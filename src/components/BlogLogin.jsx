f1d7bb41d46d1408d610bac7c9a4f4c86f35eca4
Diyarose <dhiyarosedavis@gmail.com>
2024-06-13 14:42:59 +0530

    signapi

--- src/components/BlogLogin.jsx
+++ src/components/BlogLogin.jsx
@@ -1,6 +1,41 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const BlogLogin = () => {
+    const [data,setData]=useState(
+        {
+            
+                
+                "email":"",
+                "pass":""
+            
+        }
+    )
+const inputHandler=(event)=>{
+    setData({...data,[event.target.name]:event.target.value})
+}
+const readValue=()=>{
+    
+    console.log(data)
+    axios.post("http://localhost:8080/login",data).then(
+        (response)=>{
+            console.log(response.data)
+            if(response.data.status=="success")
+                {
+                    alert("succesfully added")
+                }
+                else{
+                    alert("error")
+                }
+        }
+    ).catch(
+        (error)=>{
+            console.log(error.message)
+            alert(error.message)
+        }
+    )
+}
   return (
     <div>
         <div className="container">
@@ -10,14 +45,17 @@ const BlogLogin = () => {
                         
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Email</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Password</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='pass' value={data.pass} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-primary">Submit</button>
+                            <button className="btn btn-primary" onClick={readValue}>Submit</button>
+                        </div>
+                        <div>
+                            <Link class="nav-link" to="/signin">Sign In</Link>
                         </div>
                     </div>
                 </div>
