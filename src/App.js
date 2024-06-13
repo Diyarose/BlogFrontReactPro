0559ce07bd4533de2644f938bf81b929f9f9d85d
Diyarose <dhiyarosedavis@gmail.com>
2024-06-13 14:01:51 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import BlogLogin from './components/BlogLogin';
+import BlogSign from './components/BlogSign';
 
 function App() {
   return (
     <div>
       <BlogLogin/>
+      <BlogSign/>
     </div>
   );
 }
